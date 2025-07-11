5883f8aab29110f65e1c97b9b0cfe4322b505f9b
DevSourcerer04 <builder4107@gmail.com>
2025-07-12 05:09:04 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aviation_crash',
+    version='0.0.1',
+    author='Abhishek Gupta',
+    author_email='builder4107@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'matplotlib',
+        'seaborn',
+        'plotly',
+        'xgboost',
+        'lightgbm',
+        'statsmodels',
+        'prophet',
+        'scipy',
+        'jupyter',
+        'notebook',
+        'folium',
+        'joblib',
+        'pyyaml',
+        'tqdm'
+    ]
+)
